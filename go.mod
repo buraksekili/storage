a96125b691277682e0c94db32ab31374fbb8ad6f
Burak Sekili <buraksekili@gmail.com>
2021-02-08 23:12:56 +0300

    added testing for image uploading

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.3 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/stretchr/testify v1.7.0
 	google.golang.org/grpc v1.35.0
 	google.golang.org/protobuf v1.25.0
 )
--- service/uploader_server_test.go
+++ service/uploader_server_test.go
@@ -1,7 +1,109 @@
 package service_test
 
-import "testing"
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/buraksekili/storage/proto/pb"
+	"google.golang.org/grpc"
+
+	"github.com/buraksekili/storage/service"
+)
 
 func TestUploaderServer_UploadImage(t *testing.T) {
+	t.Parallel()
 
+	testStoragePath := "../img/test-client"
+	testOutputPath := "../img/test-server"
+
+	l := log.New(os.Stdout, "[TEST] ", log.LstdFlags)
+	storage := service.NewLocalImgStorage(testOutputPath, l)
+
+	testServAddr := startTestServer(t, storage)
+	client := getTestClient(t, testServAddr)
+
+	imgName := "test"
+	ext := "png"
+	fullImageName := fmt.Sprintf("%s.%s", imgName, ext)
+
+	cwd, err := os.Getwd()
+	require.NoError(t, err)
+	fullTestImagePath := filepath.Join(cwd, testStoragePath, fullImageName)
+	fullOutputImagePath := filepath.Join(cwd, testOutputPath, fullImageName)
+
+	f, err := os.Open(fullTestImagePath)
+	require.NoError(t, err)
+
+	defer f.Close()
+	require.FileExists(t, fullTestImagePath)
+
+	stream, err := client.UploadImage(context.Background())
+	require.NoError(t, err)
+
+	req := &pb.UploadImageRequest{
+		Data: &pb.UploadImageRequest_Info{
+			Info: &pb.ImageInfo{ImageName: imgName, ImageExtension: ext},
+		},
+	}
+
+	err = stream.Send(req)
+	require.NoError(t, err)
+
+	send := 0
+
+	// read image (fn)
+	reader := bufio.NewReader(f)
+	chunk := make([]byte, 1000000)
+	for {
+		n, err := reader.Read(chunk)
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
+
+		send += n
+
+		req := &pb.UploadImageRequest{
+			Data: &pb.UploadImageRequest_ImageChunk{ImageChunk: chunk[:n]},
+		}
+
+		err = stream.Send(req)
+		require.NoError(t, err)
+	}
+
+	res, err := stream.CloseAndRecv()
+	require.NoError(t, err)
+	require.NotZero(t, res.GetImageSize())
+	require.EqualValues(t, send, res.GetImageSize())
+	require.FileExists(t, fullOutputImagePath)
+	require.NoError(t, os.Remove(fullOutputImagePath))
+}
+
+func getTestClient(t *testing.T, addr string) pb.ImageUploaderClient {
+	cc, err := grpc.Dial(addr, grpc.WithInsecure())
+	require.NoError(t, err)
+	return pb.NewImageUploaderClient(cc)
+}
+
+func startTestServer(t *testing.T, storage *service.LocalImgStorage) string {
+	us := service.NewUploaderServer(storage)
+
+	gRPCServ := grpc.NewServer()
+	pb.RegisterImageUploaderServer(gRPCServ, us)
+
+	lis, err := net.Listen("tcp", "")
+	require.NoError(t, err)
+
+	go gRPCServ.Serve(lis)
+
+	return lis.Addr().String()
 }
